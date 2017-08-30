6dc0ad9ef49ecac45dbd9ce2fefb9f2013845ddd
liuxf <liuxf@taofen8.com>
2017-08-30 17:20:58 +0800

    bug fix

--- src/main/java/com/oversea/task/service/TaskService.java
+++ src/main/java/com/oversea/task/service/TaskService.java
@@ -18,4 +18,6 @@ public interface TaskService {
 	public TaskResult CheckGiftCard(Task task);//爬去礼品卡余额
 	
 	public TaskResult productOrderCheckService(Task task);//校验商品是否可以售卖
+	
+	public TaskResult manualShip(Task task);//手动处理订单
 }
